3877bfce8b87f455fd14f32a2e0dfbe167c40489
James Eneh <erozonachi.james@gmail.com>
2019-10-07 18:06:17 +0100

    Find next smallest element - selection_sort()

--- sorting/src/iterative_sorting/iterative_sorting.py
+++ sorting/src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,31 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        smallest_index = i
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        # (hint, can do in 3 loc)
+        smallest_element = arr[smallest_index]
+        for j in range(i+1, len(arr)):
+            if arr[j] < smallest_element:
+                smallest_element = arr[j]
+                smallest_index = j
 
         # TO-DO: swap
-
-
-
 
     return arr
 
 
+print(selection_sort([5, 8, 2, 6, 9, 4, 3, 7, 1]))
+
+
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
