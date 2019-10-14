ca5a7c07f677b1ee16f941642853d76038eca027
James Eneh <erozonachi.james@gmail.com>
2019-10-14 17:47:44 +0100

    Add logic for dll move_to_front method

--- data-structures/doubly_linked_list/doubly_linked_list.py
+++ data-structures/doubly_linked_list/doubly_linked_list.py
@@ -114,7 +114,9 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
