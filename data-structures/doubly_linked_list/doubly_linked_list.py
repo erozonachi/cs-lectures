52ffc84a7488303163a0a11653539cda50b57e4a
James Eneh <erozonachi.james@gmail.com>
2019-10-14 17:31:34 +0100

    Add logic for dll add_to_tail method

--- data-structures/doubly_linked_list/doubly_linked_list.py
+++ data-structures/doubly_linked_list/doubly_linked_list.py
@@ -66,6 +66,7 @@ class DoublyLinkedList:
         self.head = new_node
         if current_head:
             current_head.prev = self.head
+        self.length += 1
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
@@ -77,12 +78,25 @@ class DoublyLinkedList:
         current_head = self.head
         self.head = current_head.next
         self.head.prev = current_head.prev
+        self.length -= 1;
+
+        return current_head
 
    """Wraps the given value in a ListNode and inserts it
     as the new tail of the list. Don't forget to handle
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        if self.head == None and self.tail == None:
+            new_node = ListNode(value)
+            self.head, self.tail = new_node
+            self.length += 1
+            return True
+        current_tail = self.tail
+        new_node = ListNode(value, current_tail, current_tail.next)
+        self.tail = new_node
+        if current_tail:
+            current_tail.next = self.tail
+        self.length += 1
 
     """Removes the List's current tail node, making the
     current tail's previous node the new tail of the List.
