a651244d65a6e93873b1a85581cec10b4deee7b2
James Eneh <erozonachi.james@gmail.com>
2019-10-14 17:49:36 +0100

    Add logic for dll move_to_end method

--- data-structures/doubly_linked_list/doubly_linked_list.py
+++ data-structures/doubly_linked_list/doubly_linked_list.py
@@ -121,7 +121,9 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
