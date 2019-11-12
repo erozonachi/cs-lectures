eaf49cfc2566f288f647fbc8381681e37fcbae1f
James Eneh <erozonachi.james@gmail.com>
2019-11-12 16:49:35 +0100

    Added cpu functionality

--- computer-architecture/ls8/cpu.py
+++ computer-architecture/ls8/cpu.py
@@ -1,65 +1,113 @@
 """CPU functionality."""
-
 import sys
+​
+# Instruction Set
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+​
+
 
 class CPU:
     """Main CPU class."""
 
-    def __init__(self):
+
+​
+   def __init__(self):
         """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
         pass
-
-    def load(self):
-        """Load a program into memory."""
-
-        address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
-
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
-
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+​
+   def ram_read(self, addr):
+        if addr <= 255:
+            return self.ram[addr]
         else:
-            raise Exception("Unsupported ALU operation")
-
-    def trace(self):
+            return f"ERROR: Unable to read --> address {addr} out of bounds!"
+​
+   def ram_write(self, value, addr):
+        if addr <= 255:
+            self.ram[addr] = value
+        else:
+            return f"ERROR: Unable to write {value} --> address {addr} out of bounds!"
+​
+   def load(self):
+        """Load a program into memory."""
+​
+        address = 0
+​
+        # For now, we've just hardcoded a program:
+        ​
+        program = [
+                # From print8.ls8
+                0b10000010,  # LDI R0,8
+                0b00000000,
+                0b00001000,
+                0b01000111,  # PRN R0
+                0b00000000,
+                0b00000001,  # HLT
+            ]
+        ​
+        for instruction in program:
+                self.ram[address] = instruction
+                address += 1
+​
+​
+   def alu(self, op, reg_a, reg_b):
+        """ALU operations."""
+​
+        if op == "ADD":
+                self.reg[reg_a] += self.reg[reg_b]
+            # elif op == "SUB": etc
+            else:
+                raise Exception("Unsupported ALU operation")
+​
+   def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
+​
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
-
+                self.pc,
+                # self.fl,
+                # self.ie,
+                self.ram_read(self.pc),
+                self.ram_read(self.pc + 1),
+                self.ram_read(self.pc + 2)
+            ), end='')
+        ​
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
-
+                print(" %02X" % self.reg[i], end='')
+        ​
         print()
-
-    def run(self):
+​
+   def run(self):
         """Run the CPU."""
-        pass
+        # read the memory address that's stored in register PC,
+        # and store that result in IR
+        running = True
+        while running:
+            # FETCH
+            IR = self.ram[self.pc]
+​
+           # DECODE
+        if IR == HLT:
+                # EXECUTE
+                # print("CPU Halted...")
+                self.pc += 1
+                running = False
+        elif IR == LDI:
+            # EXECUTE
+            reg = self.ram_read(self.pc + 1)
+            val = self.ram_read(self.pc + 2)
+            self.reg[reg] = val
+            self.pc += 3
+            # print(f"LDI: Reg->{reg}, Val->{val}")
+        elif IR == PRN:
+            # EXECUTE
+            reg = self.ram_read(self.pc + 1)
+            val = self.reg[reg]
+            print(val)
+            self.pc += 2
